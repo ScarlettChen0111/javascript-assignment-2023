12e4028174dd4d8bca9617cb9dc1dbb75f95498b
ScarlettChen0111 <123611265+ScarlettChen0111@users.noreply.github.com>
2023-03-23 16:42:42 -0400

    Add files via upload

---
+++ class5/class5.js
@@ -0,0 +1,4 @@
+<form name="search" action="https://google.com/search" id="searchForm" method="GET">
+  <input type="text" name="searchBox"></input>
+  <button type="submit">Search</button>
+</form>
